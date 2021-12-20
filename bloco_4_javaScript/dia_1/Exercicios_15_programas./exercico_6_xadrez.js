36814fa8a6ee47c695ddc869a7fb9d7dad780645
Rogério Ferreira <rogerioferreiraeu@gmail.com>
2021-12-20 19:02:10 -0300

    exercicio jogo xadrez e mega sena

---
+++ bloco_4_javaScript/dia_1/Exercicios_15_programas./exercico_6_xadrez.js
@@ -0,0 +1,17 @@
+/**Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+let pecaDeXadrez = peao;
+
+if (pecaDeXadrez === peao) {
+  console.log("horizontal e vertical");
+} else if (pecaDeXadrez === rainha) {
+  console.log("Horizontal, vertical e diagoanal");
+} else if (pecaDeXadrez === peao) {
+  console.log("2 casas pra frente na primeira jogada e 1 nas demais.");
+} else {
+  console.log("não tem peça");
+}
+*/
+
+
+let cor = vermelho;
