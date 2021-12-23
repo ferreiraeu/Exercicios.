f5e362f12f9e56d607d01f35e8660282d9105e7e
Rogério Ferreira <rogerioferreiraeu@gmail.com>
2021-12-23 00:03:49 -0300

    soma e media com array

---
+++ bloco_4_javaScript/dia_2/Exercicios_09_java_array_loopFor.js/exercicio2_array.js
@@ -0,0 +1,10 @@
+/** some todos os valores contidos no array e imprima o resultado;
+*/
+let soma = 0;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    for(let index = 0; index < numbers.length; index += 1){
+        soma += (numbers [index]);
+    }
+
+    console.log(soma);
+
