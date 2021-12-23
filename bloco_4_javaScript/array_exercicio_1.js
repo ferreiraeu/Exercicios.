278ae036ef09e936b4a39ef85025b7cc814f3747
Rogério Ferreira <rogerioferreiraeu@gmail.com>
2021-12-22 21:48:34 -0300

    criação de exescicios for e array, e pasta 9 exercicios

---
+++ bloco_4_javaScript/array_exercicio_1.js
@@ -0,0 +1,4 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let imprimir of names) {
+    console.log("Eu conheço ", imprimir ,"é uma pessoa super gente boa");
+}
---
+++ bloco_4_javaScript/dia_2/for_of _array _ex3.js
@@ -0,0 +1,6 @@
+
+let arrOfNumbers = [110, 20, 30];
+for (let sum of arrOfNumbers) {
+  sum += 1; // sum é o valor da posição da array + 1 , então posição 0 = 10 +1 = 11
+  console.log(sum);
+}
