de5ff7a959aaeace3284bd33e472009cef6009ab
Rogério Ferreira <rogerioferreiraeu@gmail.com>
2021-12-27 19:32:54 -0300

    exercicios objetos e funções

---
+++ bloco_4_javaScript/dia_4_Objetos e funções/funções_Status_Logado_Deslogado.js
@@ -0,0 +1,28 @@
+/**let status = 'deslogado';
+
+function logarDeslogar() {
+  if (status === 'deslogado') {
+    status = 'logado';
+  } else {
+    status = 'deslogado';
+  }
+}
+
+console.log(status); // deslogado
+
+logarDeslogar();
+console.log(status); // logado
+
+logarDeslogar();
+console.log(status); // deslogado
+
+console.log('O usuário está ' + status); // O usuário está deslogado
+ */
+
+
+let a = "Serei a melhor pessoa dev";
+let b = "Esse é meu jeito dev de ser!";
+let num = 2;
+let strings = [a, b];
+
+console.log((typeof num) == (typeof 'number'));
