b1fc70151773314373fc23a31a738b5b3a998013
Rogério Ferreira <rogerioferreiraeu@gmail.com>
2021-12-27 20:32:54 -0300

    Criação da pasta Doom do bloco 5 e primeiro exemplo do video sobre getelementeByid

---
+++ bloco_5_DOM./doom1_getElementById.js
@@ -0,0 +1,7 @@
+
+
+
+//document.getElementById("teste"); // esta acessando , porem não aparece 
+console.log(document.getElementById("teste2"));
+
+document.getElementById("teste").innerText = "eu quero ver mar pegar fogo"  // esta acessando , porem não aparece 
