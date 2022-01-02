3643631e5ec25351ce7442b63b74e1edcb5e262e
Rogério Ferreira <rogerioferreiraeu@gmail.com>
2022-01-01 23:58:56 -0300

    Eventos add event list

---
+++ bloco_5_DOM./trabalhando_com_elementos.js
@@ -0,0 +1,20 @@
+
+
+
+let title = document.getElementById('title') 
+// pegueio o elemento 
+//depois tranformei em uma constante colocando uma Let title = 
+
+
+
+ let novoElemento = document.createElement('div')
+// criei uma DIV . 
+//vamos criar uma constante támbem de nosso create elemente
+// vamos transformar nosso elemento que pegamos getelemente.id em uma constante
+title.appendChild(novoElemento)
+
+newElement.className = "circle";
+
+let titulo 
+
+
