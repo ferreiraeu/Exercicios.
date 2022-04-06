e535d45f01e104b6625765d72b23809d19e641cf
Rogério Ferreira <rogerioferreiraeu@gmail.com>
2022-04-05 23:56:05 -0300

    atualizando

--- Bloco_07_JavaScript_ES6_Testes-Unitários/NodeJS Assert/NodeJS Assert.js
+++ Bloco_07_JavaScript_ES6_Testes-Unitários/NodeJS Assert/NodeJS Assert.js
@@ -2,3 +2,4 @@ const assert = require('assert'); // Sintaxe para incluir o módulo assert
 
 assert.strictEqual(50, 50); // Sem erros: 50 === 50
 assert.strictEqual(50, 70); // AssertionError: 50 !== 70
+// Xablau
